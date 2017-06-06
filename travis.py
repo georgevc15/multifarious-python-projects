03eb9ff2f8647e7050fda3d052f11ec92d0f4644
George Codat Tudoran <office@solcreation.ro>
2017-06-06 22:48:08 +0300

    Removing users from the list

--- travis.py
+++ travis.py
@@ -1,13 +1,16 @@
 known_users = ["Alice","Bob","Claire","Dan","Emma","Fred","Georgie","Harry"]
 
-print(len(known_users))
-
-
 while True:
     print("Hi! My name is Travis")
     name = input('What is your name?: ').strip().capitalize()
 
     if name in known_users:
-        print("name recognised")
+        output = "Hello {}".format(name)
+        print(output)
+        remove = input("Would you like to be removed from the system (y/n): ").lower();
+
+        if remove == "y":
+            known_users.remove(name)
+        
     else:
         print("name NOT recognised")
