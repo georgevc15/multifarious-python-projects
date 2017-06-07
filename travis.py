4d9dd2d831eb0c9d5a6ea91ca81108edd204788e
George Codat Tudoran <office@solcreation.ro>
2017-06-07 23:07:07 +0300

    Adding users to list

--- travis.py
+++ travis.py
@@ -7,10 +7,17 @@ while True:
     if name in known_users:
         output = "Hello {}".format(name)
         print(output)
-        remove = input("Would you like to be removed from the system (y/n): ").lower();
+        remove = input("Would you like to be removed from the system (y/n): ").strip().lower();
 
         if remove == "y":
             known_users.remove(name)
+        elif remove == "n":
+            print("No problem, I dindn't want you to leave anyway!")
         
     else:
-        print("name NOT recognised")
+        print("Hmmmm I don't think I have met you yet {}".format(name))
+        add_me = input("Would you like to be added to the system (y/n)?: ").strip().lower();
+        if add_me == "y":
+            known_users.append(name)
+        elif add_me == "n":    
+            print("No worries, see you around!")
