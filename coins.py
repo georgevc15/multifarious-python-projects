42fd2bf8d727d59cdad8b4ca910a1df4232301cf
George Codat Tudoran <office@solcreation.ro>
2017-06-24 02:51:29 +0300

    Creating coin template

---
+++ coins.py
@@ -0,0 +1,37 @@
+import random
+
+class Lei:
+    
+    def __init__(self, rare=False):
+
+        self.rare = rare
+
+        if self.rare:
+            self.value = 1.25
+        else:
+            self.value = 1.00
+
+        self.color = "gold"
+        self.num_edges = 1
+        self.diamater = 22.5 # mm
+        self.thickness = 3.15 # mm
+        self.heads = True # True if heads
+
+    def rust(self):
+        self.color = "greenish"
+
+    def clean(self):
+        self.color = "gold"
+
+    def flip(self):
+        heads_options = [True,False]
+        choice = random.choice(heads_options)
+        self.heads = choice
+
+    def __del__(self):
+        print("Coin Spent!")
+
+
+
+
+
