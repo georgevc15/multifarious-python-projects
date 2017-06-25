ca8fa7d19f14043d3c8dd8de8b19cc6f18cff0bc
George Codat Tudoran <office@solcreation.ro>
2017-06-26 01:38:55 +0300

    Making shiny coins

--- coins.py
+++ coins.py
@@ -36,9 +36,119 @@ class Coin:
             choice = random.choice(heads_options)
             self.heads = choice
 
+    def __str__(self):
+        if self.original_value >= 1:
+            return "£{} Coin".format(int(self.original_value))
+        else:
+            return "{}p Coin".format(int(self.original_value * 100))
 
 
-class Lei(Coin):
+class One_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value":0.01,
+            "clean_colour":"bronze",
+            "rusty_colour":"brownish",
+            "num_edges":1,
+            "diameter":20.3,
+            "thickness":1.52,
+            "mass":3.56
+            }
+        super().__init__(**data)
+
+
+class Two_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value":0.02,
+            "clean_colour":"bronze",
+            "rusty_colour":"brownish",
+            "num_edges":1,
+            "diameter":25.9,
+            "thickness":1.85,
+            "mass":7.12
+            }
+        super().__init__(**data)
+
+
+class Five_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value":0.05,
+            "clean_colour":"silver",
+            "rusty_colour":None,
+            "num_edges":1,
+            "diameter":18.0,
+            "thickness":1.77,
+            "mass":3.25
+            }
+        super().__init__(**data)
+
+        def rust(self):
+            self.colour = self.clean_colour
+
+        def clean(self):
+            self.colour = self.clean_colour
+        
+class Ten_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value":0.10,
+            "clean_colour":"silver",
+            "rusty_colour":None,
+            "num_edges":1,
+            "diameter":24.5,
+            "thickness":1.85,
+            "mass":6.50
+            }
+        super().__init__(**data)
+
+        def rust(self):
+            self.colour = self.clean_colour
+
+        def clean(self):
+            self.colour = self.clean_colour
+
+class Twenty_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value":0.20,
+            "clean_colour":"silver",
+            "rusty_colour":None,
+            "num_edges":7,
+            "diameter":21.4,
+            "thickness":1.7,
+            "mass":5.00
+            }
+        super().__init__(**data)
+
+        def rust(self):
+            self.colour = self.clean_colour
+
+        def clean(self):
+            self.colour = self.clean_colour
+
+
+class Fifty_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value":0.50,
+            "clean_colour":"silver",
+            "rusty_colour":None,
+            "num_edges":7,
+            "diameter":27.3,
+            "thickness":1.78,
+            "mass":8.00
+            }
+        super().__init__(**data)
+
+        def rust(self):
+            self.colour = self.clean_colour
+
+        def clean(self):
+            self.colour = self.clean_colour
+
+class One_Pound(Coin):
     def __init__(self):
         data = {
             "original_value":1.00,
@@ -52,8 +162,27 @@ class Lei(Coin):
         super().__init__(**data)
 
 
+class Two_Pound(Coin):
+    def __init__(self):
+        data = {
+            "original_value":2.00,
+            "clean_colour":"gold & silver",
+            "rusty_colour":"greenish",
+            "num_edges":1,
+            "diameter":28.4,
+            "thickness":2.50,
+            "mass":12.00
+            }
+        super().__init__(**data)
 
-        
-       
 
+coins = [One_Pence(), Two_Pence(), Five_Pence(), Ten_Pence(), Twenty_Pence(),
+         Fifty_Pence(), One_Pound(), Two_Pound()]
+
+for coin in coins:
+    arguments = [coin, coin.colour, coin.value, coin.diameter, coin.thickness,
+                 coin.num_edges, coin.mass]
+
+    string = "{} - Colour: {}, value: {}, diameter(mm): {}, thickness(mm):{}, number of edges:{}, mass(g): {}".format(*arguments)
+    print(string)
 
