e3d10ba83702de07a246b9c359645cb8d553fa04
George Codat Tudoran <office@solcreation.ro>
2017-06-25 05:14:47 +0300

    Making new coin

---
+++ Readme.md
@@ -0,0 +1,6 @@
+For new coin use the class Lei example
+
+Instantiation of the class: one_lei_coin = Lei()
+
+make the coin rusty: one_lei_coin.rusty()
+
--- coins.py
+++ coins.py
@@ -1,37 +1,59 @@
 import random
 
-class Lei:
-    
-    def __init__(self, rare=False):
 
-        self.rare = rare
+class Coin:
+    def __init__(self, rare=False, clean = True, heads = True, **kwargs):
 
-        if self.rare:
-            self.value = 1.25
+        for key,value in kwargs.items():
+          setattr(self,key,value)
+
+        self.is_rare =  rare
+        self.is_clean = clean
+        self.heads = heads
+
+
+        if self.is_rare:
+            self.value = self.original_value * 1.25
         else:
-            self.value = 1.00
+            self.value = self.original_value
 
-        self.color = "gold"
-        self.num_edges = 1
-        self.diamater = 22.5 # mm
-        self.thickness = 3.15 # mm
-        self.heads = True # True if heads
+        if self.is_clean:
+            self.colour = self.clean_colour
+        else:
+            self.colour = self.rusty_colour
 
     def rust(self):
-        self.color = "greenish"
+            self.colour = self.rusty_colour
 
     def clean(self):
-        self.color = "gold"
-
-    def flip(self):
-        heads_options = [True,False]
-        choice = random.choice(heads_options)
-        self.heads = choice
+            self.colour = self.clean_colour
 
     def __del__(self):
-        print("Coin Spent!")
+            print("Coin Spent!")
+            
+    def flip(self):
+            heads_options = [True,False]
+            choice = random.choice(heads_options)
+            self.heads = choice
 
 
 
+class Lei(Coin):
+    def __init__(self):
+        data = {
+            "original_value":1.00,
+            "clean_colour":"gold",
+            "rusty_colour":"greenish",
+            "num_edges":1,
+            "diameter":22.5,
+            "thickness":3.15,
+            "mass":9.5
+            }
+        super().__init__(**data)
+
+
+
+        
+       
 
 
