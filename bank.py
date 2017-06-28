aebf7355ad886e6f9357921eb5630c6f6d25177e
George Codat Tudoran <office@solcreation.ro>
2017-06-28 03:21:54 +0300

    Current and Savings accounts

--- bank.py
+++ bank.py
@@ -4,10 +4,35 @@ class Account:
         self.balance = balance
         self.min_balance = min_balance
 
-
     def deposit(self,amount):
         self.balance += amount
 
     def widraw(self,amount):
         if self.balance - amount >= self.min_balance:
             self.balance -=amount
+        else:
+            print("Sorry not enough funds!")
+
+    def statement(self):
+        print("Accoount Balance: £{}".format(self.balance))
+
+
+class Current(Account):
+    def __init__(self,name,balance):
+        super().__init__(name,balance,min_balance =- 1000)
+
+    def __str__(self):
+        return "{} Current Account: Balance £{}".format(self.name, self.balance)
+
+
+class Savings(Account):
+    def __init__(self,name,balance):
+        super().__init__(name,balance,min_balance = 0)
+
+    def __str__(self):
+        return "{} Savings Account: Balance £{}".format(self.name, self.balance)
+
+
+
+    
+    
